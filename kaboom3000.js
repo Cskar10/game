364938211240a192619d969e6124d3e52e665e9a
Cskar10 <skarakischris@gmail.com>
2025-10-20 12:23:42 +1100

    Update kaboom3000.js

--- kaboom3000.js
+++ kaboom3000.js
@@ -1,3 +1,3 @@
 /* esm.sh - kaboom@3000.0.0-beta.3 */
-export * from "/kaboom@3000.0.0-beta.3/es2022/kaboom.mjs";
-export { default } from "/kaboom@3000.0.0-beta.3/es2022/kaboom.mjs";
+export * from "/kaboom@3000.0.0-beta.3/es2022/kaboom3000.js";
+export { default } from "/kaboom@3000.0.0-beta.3/es2022/kaboom3000.js";
